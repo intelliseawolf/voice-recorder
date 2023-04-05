a8928ef84b4225cd08717e91646f3a2ee25a4a14
intellieseawolf <intelliseawolf103@gmail.com>
2023-04-05 15:23:43 -0400

    added modal to the webcam

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,13 @@ import React, {
   useCallback,
 } from "react";
 import Webcam from "react-webcam";
-import { Button } from "react-bootstrap";
+import { Button, Modal } from "react-bootstrap";
 import "./App.css";
 
 import { useAudioRecorder } from "react-audio-voice-recorder";
 
 const WebcamCapture: React.FC = () => {
+  const [show, setShow] = useState(false);
   const webcamRef = useRef<Webcam>(null);
   const [imgSrc, setImgSrc] = useState<string | null>(null);
 
@@ -20,10 +21,30 @@ const WebcamCapture: React.FC = () => {
     setImgSrc(imageSrc || null);
   }, [webcamRef, setImgSrc]);
 
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <>
-      <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
-      <Button onClick={capture}>Capture photo</Button>
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button onClick={capture}>Capture photo</Button>
+        </Modal.Footer>
+      </Modal>
+
       {imgSrc && <img src={imgSrc} alt="captured" />}
     </>
   );
@@ -51,19 +72,21 @@ const App = () => {
 
   return (
     <div className="App">
-      {isRecording ? (
-        <>
-          {isPaused ? (
-            <Button onClick={togglePauseResume}>resume</Button>
-          ) : (
-            <Button onClick={togglePauseResume}>pause</Button>
-          )}
-          <Button onClick={stopRecording}>stop</Button>
-        </>
-      ) : (
-        <Button onClick={startRecording}>play</Button>
-      )}
-      <audio src={audioSrc} controls></audio>
+      <div>
+        {isRecording ? (
+          <>
+            {isPaused ? (
+              <Button onClick={togglePauseResume}>resume</Button>
+            ) : (
+              <Button onClick={togglePauseResume}>pause</Button>
+            )}
+            <Button onClick={stopRecording}>stop</Button>
+          </>
+        ) : (
+          <Button onClick={startRecording}>play</Button>
+        )}
+        <audio src={audioSrc} controls></audio>
+      </div>
       <WebcamCapture />
     </div>
   );
